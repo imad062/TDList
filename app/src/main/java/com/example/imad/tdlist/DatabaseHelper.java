4e50e1c498ff0245c6e867abbc93c862acc6525d
imad062 <nooruddinimad@gmail.com>
2018-01-17 20:45:33 +0600

    created alertDialog for getting task details

---
+++ app/src/main/java/com/example/imad/tdlist/DatabaseHelper.java
@@ -0,0 +1,4 @@
+package com.example.imad.tdlist;
+
+public class DatabaseHelper {
+}
--- app/src/main/java/com/example/imad/tdlist/MainActivity.java
+++ app/src/main/java/com/example/imad/tdlist/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.imad.tdlist;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -61,9 +65,33 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void addClicked()
+    public void addClicked(View view)
     {
-        AlertDialog.Builder popup = new AlertDialog.Builder(this);
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
 
+        TextView title = new TextView(this);
+        title.setText("Add Task");
+        title.setGravity(Gravity.CENTER);
+        title.setPadding(10, 10, 10, 10);
+        title.setTextSize(20);
+        title.setTypeface(null, Typeface.BOLD);
+
+        alert.setCustomTitle(title)
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "ADDED", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "CANCELLED", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .setView(R.layout.alert_edittext);
+
+        AlertDialog alertDialog = alert.create();
+        alertDialog.show();
     }
 }
